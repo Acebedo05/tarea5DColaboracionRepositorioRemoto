b45851cf55803bfdd4c5329e7e4f356dfa63d0d4
jaceara2006 <jaceara2006@g.educaand.es>
2024-01-15 09:54:54 +0100

    Añade constructores en clase modelo

--- src/main/java/daw/Producto.java
+++ src/main/java/daw/Producto.java
@@ -10,5 +10,14 @@ public class Producto {
     String marca;
     int precio;
     double descuento;
-    
+
+    public Producto(String marca, int precio, double descuento) {
+        this.marca = marca;
+        this.precio = precio;
+        this.descuento = descuento;
+    }
+
+    public Producto() {
+
+    }
 }
