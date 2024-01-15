be908c95c7af0e4f28d190047f4680fedba15900
jaceara2006 <jaceara2006@g.educaand.es>
2024-01-15 10:02:23 +0100

    Añade getters y setters en clase modelo

--- src/main/java/daw/Producto.java
+++ src/main/java/daw/Producto.java
@@ -7,9 +7,9 @@ package daw;
 public class Producto {
 
     // Atributos.
-    String marca;
-    int precio;
-    double descuento;
+    private String marca;
+    private int precio;
+    private double descuento;
 
     public Producto(String marca, int precio, double descuento) {
         this.marca = marca;
@@ -20,4 +20,30 @@ public class Producto {
     public Producto() {
 
     }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+    
+    
 }
