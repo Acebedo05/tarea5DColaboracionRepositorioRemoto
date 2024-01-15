b370eda3d01d60ce00a2444c50796737233d913a
adrianviana <aviagar307@g.educaand.es>
2024-01-15 10:10:14 +0100

    Añade objetos en clase main

---
+++ src/main/java/daw/Main.java
@@ -0,0 +1,20 @@
+package daw;
+
+/**
+ *
+ * @author adrian
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Producto p1 = new Producto("pagani", 100_000, 0);
+        Producto p2 = new Producto();
+        
+        
+        System.out.println(p1);
+        System.out.println(p2);
+        
+    }
+
+}
